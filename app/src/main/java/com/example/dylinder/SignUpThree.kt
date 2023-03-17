344af2d99de6b0051b8a54660bbb15dbd4353976
Thuvarakan <thuvarakanp96@gmail.com>
2023-03-17 17:11:26 +0530

    Dylinder login and signup screens

---
+++ app/src/main/java/com/example/dylinder/SignUpThree.kt
@@ -0,0 +1,11 @@
+package com.example.dylinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpThree : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_three)
+    }
+}

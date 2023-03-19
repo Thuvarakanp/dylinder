415b214c5d00850e25c0cbdcc078d1d5c01bfca0
Thuvarakan <thuvarakanp96@gmail.com>
2023-03-19 21:17:17 +0530

    third commit with payment and seller

---
+++ app/src/main/java/com/example/dylinder/AddProducts.kt
@@ -0,0 +1,11 @@
+package com.example.dylinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddProducts : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_products)
+    }
+}

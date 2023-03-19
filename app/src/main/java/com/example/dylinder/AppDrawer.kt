f8455068c74a5b003df5e11ac204509423a55161
Thuvarakan <thuvarakanp96@gmail.com>
2023-03-19 17:54:35 +0530

    Its contains majarity of xml file

---
+++ app/src/main/java/com/example/dylinder/AppDrawer.kt
@@ -0,0 +1,11 @@
+package com.example.dylinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppDrawer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_drawer)
+    }
+}
